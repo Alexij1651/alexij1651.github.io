474705d3e3eb800f068228dbaef56b553d35b355
Alejandro Ibáñez Jurado <ibaneza05@gmail.com>
2026-04-27 19:05:05 +0200

    js bombilla

--- JS/js/bombilla.js
+++ JS/js/bombilla.js
@@ -1,17 +1,16 @@
-
 let encendida = false;
 
-function interBombilla(){
+function toggleBombilla(){
 
     let bombilla = document.getElementById("bombilla");
     let boton = document.getElementById("boton");
 
     if(encendida){
-        bombilla.src = "";
+        bombilla.src = "https://www.w3schools.com/js/pic_bulboff.gif";
         boton.textContent = "Encender";
         encendida = false;
     }else{
-        bombilla.src = "";
+        bombilla.src = "https://www.w3schools.com/js/pic_bulbon.gif";
         boton.textContent = "Apagar";
         encendida = true;
     }
